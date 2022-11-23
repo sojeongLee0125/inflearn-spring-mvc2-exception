3d5390d3ad8bcbbd4fa1f2a14609c0fabfb59992
sojeonglee <sojeonglee0125@gmail.com>
2022-11-23 21:57:47 +0900

    feat : 인터셉터 - 에러페이지 요청 처리 적용 설정 추가 (에러요청은 제외)

--- src/main/java/spring/exception/WebConfig.java
+++ src/main/java/spring/exception/WebConfig.java
@@ -3,8 +3,10 @@ package spring.exception;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spring.exception.filter.LogFilter;
+import spring.exception.interceptor.LogInterceptor;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -12,7 +14,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
