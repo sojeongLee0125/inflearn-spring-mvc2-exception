e55d6ba1c6ac5d32a017ee0164c4cbccb3e2184b
sojeonglee <sojeonglee0125@gmail.com>
2022-11-28 20:59:37 +0900

    feat : ResponseStatusExceptionResolver 에러 상태 처리 코드 구현 + 메세지 소스 사용

--- src/main/java/spring/exception/api/ApiExceptionController.java
+++ src/main/java/spring/exception/api/ApiExceptionController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import spring.exception.exception.BadRequestException;
 import spring.exception.exception.UserException;
 
 @Slf4j
@@ -28,6 +29,11 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello" + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data
